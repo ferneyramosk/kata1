0f028a313ea52e9649ea8aa9c741e91b382653f6
Ferneyramos <ferneyramosk@hotmail.com>
2026-09-19 23:31:37 -0500

    Ejemplos clases

---
+++ bucles_lista.py
@@ -0,0 +1,19 @@
+# Contendor, lista, array
+lista_all = ["Maria",10,True,3.99,[1,2,3]]
+
+"""
+print(len(lista_all))     # Obtener la longitud de lista
+print(lista_all[0])       # Obtener la primera posicion
+print(lista_all[len(lista_all)-1])    # Obtener la ultima posicion
+
+for i in lista_all:
+    print(i)
+
+
+for i in range(0, len(lista_all)):
+    print(lista_all[i])
+
+"""
+
+
+print(lista_all[len(lista_all)-1][1])
